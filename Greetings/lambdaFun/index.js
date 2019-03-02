66ce5f04f91598592ba816d303a66d6dd8afdd3b
agdavid <antonio.g.david@gmail.com>
2019-03-02 17:26:40 -0500

    add condition to check request type

--- Greetings/lambdaFun/index.js
+++ Greetings/lambdaFun/index.js
@@ -4,4 +4,22 @@ module.exports = {
 
 function handler(event, context) {
 
+    var request = event.request;
+
+    /*
+    request.type
+    i)   LaunchRequest       Ex: "Open greeter"
+    ii)  IntentRequest       Ex: "Say hello to John" or "ask greeter to say hello to John"
+    iii) SessionEndedRequest Ex: "exit" or error or timeout
+    */
+
+    if (request.type === 'LaunchRequest') {
+        
+    } else if (request.type === 'IntentRequest') {
+
+    } else if (request.type === 'SessionEndedRequest') {
+
+    } else {
+        context.fail('Unknown intent type');
+    }
 }
