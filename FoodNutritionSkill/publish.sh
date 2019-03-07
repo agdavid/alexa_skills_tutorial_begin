e52a80feae2e8bfca6474b0ca4fd4681b2eece69
agdavid <antonio.g.david@gmail.com>
2019-03-06 22:35:35 -0500

    copy publish script

--- FoodNutritionSkill/publish.sh
+++ FoodNutritionSkill/publish.sh
@@ -1,3 +1,3 @@
-\rm -fr lambda_upload.zip
-zip -r lambda_upload.zip index.js food_db.json node_modules fruits_db.json
-aws lambda update-function-code --function-name foodNutriLookup --zip-file fileb://lambda_upload.zip
+\rm -rf lambda_upload.zip
+zip -r lambda_upload.zip index.js
+aws lambda update-function-code --function-name GreetingSkill --zip-file fileb://lambda_upload.zip
